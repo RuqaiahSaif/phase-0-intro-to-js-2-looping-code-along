952c84581127eaffd54b56e9e4a1c25893ba1df8
RuqaiahSaif <69811684+RuqaiahSaif@users.noreply.github.com>
2023-01-10 16:59:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+let myArray=[];
+
+let newArray = [];
+function writeCards(myArray,event){
+    
+    for(let i =0 ; i<myArray.length; i++){
+       
+        newArray.push(`Thank you, ${myArray[i]}, for the wonderful ${event} gift!`);
+       
+    }
+    console.log(newArray)
+    // return newArray
+} 
+
+function countDown(num){
+    let j = num;
+    while (j >= 0) {
+        console.log(j--);
+    }
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
+countDown(10)
